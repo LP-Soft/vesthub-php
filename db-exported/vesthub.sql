5f15619632326152a49f7c496b1e839a476795d8
Şafak Gün <99952412+SafakGun@users.noreply.github.com>
2024-10-28 23:49:41 +0300

    vesthub db exported

---
+++ db-exported/vesthub.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vesthub
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `favorites`
+--
+
+DROP TABLE IF EXISTS `favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favorites` (
+  `userID` int NOT NULL,
+  `houseID` int DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorites`
+--
+
+LOCK TABLES `favorites` WRITE;
+/*!40000 ALTER TABLE `favorites` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favorites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `houses`
+--
+
+DROP TABLE IF EXISTS `houses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `houses` (
+  `houseID` int NOT NULL,
+  `ownerID` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `district` varchar(45) DEFAULT NULL,
+  `neighborhood` varchar(45) DEFAULT NULL,
+  `street` varchar(45) NOT NULL,
+  `price` int NOT NULL,
+  `numOfBathroom` int NOT NULL,
+  `numOfBedroom` int NOT NULL,
+  `numOfRooms` varchar(45) NOT NULL,
+  `area` int NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `isSale` tinyint(1) DEFAULT NULL,
+  `floor` int NOT NULL,
+  `totalFloor` int NOT NULL,
+  `fiberInternet` tinyint NOT NULL,
+  `airConditioner` tinyint NOT NULL,
+  `floorHeating` tinyint NOT NULL,
+  `fireplace` tinyint NOT NULL,
+  `terrace` tinyint NOT NULL,
+  `satellite` tinyint NOT NULL,
+  `parquet` tinyint NOT NULL,
+  `steelDoor` tinyint NOT NULL,
+  `furnished` tinyint NOT NULL,
+  `insulation` tinyint NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `houseType` varchar(45) NOT NULL,
+  PRIMARY KEY (`houseID`),
+  UNIQUE KEY `id_UNIQUE` (`houseID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `houses`
+--
+
+LOCK TABLES `houses` WRITE;
+/*!40000 ALTER TABLE `houses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `houses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `isActive` tinyint(1) DEFAULT NULL,
+  `district` varchar(45) DEFAULT NULL,
+  `neighborhood` varchar(45) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Ali','Safak','alibabaciftligi@safakbaba44.com','','123','',0,NULL,NULL,NULL),(2,'Mahmut','Tuncer','mamo@gmail.com','5444399578','123','Istanbul',0,NULL,NULL,NULL),(3,'safak','gun','sa@c.com','54789878798798798','12','fa',1,NULL,NULL,NULL),(4,'Safak','Gun','safak@safak.com','p','123','Istanbul',1,NULL,NULL,NULL),(5,'abi','abi','abi@abi.com','1234567896','123','as',1,NULL,NULL,NULL),(6,'s','sa','sa@c.om','5855988620','123','s',1,NULL,NULL,NULL),(7,'safak','safak','safak@safak2.com','5444878940','123','IStanbul',1,NULL,NULL,NULL),(8,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa','a','a@aa.com','5234567895','a','a',1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-28 23:45:08
