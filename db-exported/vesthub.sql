91925f8429e8838fea34992fb2b2d2e57111a6fa
Şafak Gün <99952412+SafakGun@users.noreply.github.com>
2024-10-29 00:05:55 +0300

    db export
    include schema ticklendi

--- db-exported/vesthub.sql
+++ db-exported/vesthub.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `vesthub` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `vesthub`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: vesthub
@@ -133,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-28 23:45:08
+-- Dump completed on 2024-10-29  0:04:58
