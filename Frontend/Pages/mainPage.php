db756c0f1009d351991aa295a105d7b4753f8d3c
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2024-10-29 21:14:36 +0300

    fix footer alignment in mainPage and newListing

--- Frontend/Pages/mainPage.php
+++ Frontend/Pages/mainPage.php
@@ -4,31 +4,36 @@ include '../../Backend/mainPageService.php';
 include '../Components/houseCard.php';
 
 ?>
-<head>
-    <title>Main Page</title>
-    <link rel="stylesheet" href="../Styles/mainPage.css">
-</head>
-
-<body>
-    <div class="content">
-        <p class="last-houses-text">Last 5 Added Houses</p>
-        <div class="house-cards">
-            <?php
-            // Fetch all homes
-            $result = getLastFiveHouses();
-            // Loop through the result set and include homecard.php
-            if ($result && $result->num_rows > 0) {
-                while ($house = $result->fetch_assoc()) {
-                    displayHouseCard($house, 0);
-                }
-            } else {
-                echo "No homes available.";
-            }
-            ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" href="../Styles/mainPage.css">
+    </head>
+    <body>
+        <div class="content">
+            <div class="last-houses">
+                <p class="last-houses-text">Last 5 Added Houses</p>
+                <div class="house-cards">
+                    <?php
+                    // Fetch all homes
+                    $result = getLastFiveHouses();
+                    // Loop through the result set and include homecard.php
+                    if ($result && $result->num_rows > 0) {
+                        while ($house = $result->fetch_assoc()) {
+                            displayHouseCard($house, 0);
+                        }
+                    } else {
+                        echo "No homes available.";
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
+    </body>
+</html>
 
-</body>
 <?php
-include '../Components/footer.php';
 //closeConnection($conn);
+include '../Components/footer.php';
 ?>
