b370fb31ae936f618ef5be36b51f23aad011ba70
SevvalCetin <scetin@stu.khas.edu.tr>
2024-11-21 12:03:55 +0300

    houseCard is improved. All images will have same width and height.

--- Frontend/Components/houseCard.php
+++ Frontend/Components/houseCard.php
@@ -11,11 +11,22 @@ function displayHouseCard($house, $service) {
     ?>
     <div class="home-card" onclick="window.location.href='houseDetailsPage.php?id=<?php echo $house['houseID']; ?>'">
         <link rel="stylesheet" href="../Styles/houseCard.css">
+        <?php $image_paths = "../../house-images/"?>
         <?php if(!is_dir("../../house-images/".$house['houseID']."/")) { ?>
-            <img src="../../house-images/dummy.png" alt="House Image">
+            <img src= "../../house-images/dummy.png" alt="House Image">
         <?php } else { ?>
-            <img src="../../house-images/<?php echo $house['houseID']; ?>/1.png" alt="House Image">
+                    <?php if(file_exists($image_paths.$house['houseID']."/1.png")) {
+                        $image_path = $image_paths . $house['houseID'] . "/1.png";
+                    }
+                    elseif (file_exists($image_paths.$house['houseID']."/1.jpg")){
+                        $image_path = $image_paths.$house['houseID']."/1.jpg";
+                    }
+                    else{
+                        $image_path = $image_paths."dummy.png";
+                    }?>
+            <img src="<?php echo $image_path ?>" alt="House Image">
         <?php } ?>
+
         <div class="home-type-label">
             <?php if($house['isSale'] == 1) { ?>
                 <button class= "saleRentButton" style="background-color: #3ED736">Sale</button>
@@ -29,8 +40,10 @@ function displayHouseCard($house, $service) {
             <?php if($service == 0) { ?> <!-- user servisinden geliyorsa -->
                 <p><?php echo $fullAddress; ?></p>
             <?php } else { ?>
-                <button class="adminButton" onclick="event.stopPropagation(); window.location.href='adminPanel.php?reject=<?php echo $house['houseID']; ?>'" style="background-color: #FF4245">Reject</button>
-                <button class="adminButton" onclick="event.stopPropagation(); window.location.href='adminPanel.php?approve=<?php echo $house['houseID']; ?>'" style="background-color: #3ED736">Approve</button>
+                <div style="display: flex; align-items: center; gap: 10px;">
+                    <button class="adminButton" onclick="event.stopPropagation(); window.location.href='adminPanel.php?reject=<?php echo $house['houseID']; ?>'" style="background-color: #FF4245">Reject</button>
+                    <button class="adminButton" onclick="event.stopPropagation(); window.location.href='adminPanel.php?approve=<?php echo $house['houseID']; ?>'" style="background-color: #3ED736">Approve</button>
+                </div>
             <?php } ?>
         </div>
     </div>
